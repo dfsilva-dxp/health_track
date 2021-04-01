2f396d509aa08d65f0083945c1440ed75993c1ee
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-01 13:23:11 -0300

    refatoring: reestilizando a tela home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,63 +4,72 @@ export function Home() {
   return (
     <section className="container">
       <div className="content">
-        <h1>Home</h1>
-        <div className="display-info">
-          <div className="followers box">
-            <header>
-              <h4>Followers</h4>
-            </header>
-            <strong>17</strong>
-          </div>
-          <div className="following box">
-            <header>
-              <h4>Following</h4>
-            </header>
-            <strong>31</strong>
-          </div>
-          <div className="starreds box">
-            <header>
-              <h4>Starreds</h4>
-            </header>
-            <strong>20</strong>
-          </div>
-        </div>
-        <div className="display-user">
-          <div className="profile-info box">
-            <header>
-              <h4>Profile</h4>
-            </header>
-            <div className="wrap-info">
+        <header>
+          <h1>Home</h1>
+        </header>
+        <div className="display">
+          <div className="user-info">
+            <div className="avatar box">
               <img
                 src="https://avatars.githubusercontent.com/u/16245261?v=4"
                 alt="Avatar"
               />
-              <div className="info">
-                <a href="http://" target="_blank" rel="noopener noreferrer">
-                  Daniel Silva
-                </a>
-                <p>daniel-silva-dxp</p>
+            </div>
+            <div className="info box">
+              <header>
+                <h4>Biography</h4>
+              </header>
+              <div className="wrap">
+                <h1>
+                  <a href="http://" target="_blank" rel="noopener noreferrer">
+                    Daniel Silva
+                  </a>
+                </h1>
+                <h4>daniel-silva-dxp</h4>
                 <ul>
                   <li>
-                    <small>Company: </small>
+                    <strong>Company: </strong>
                     Grapeway
                   </li>
                   <li>
-                    <small>Site: </small>
+                    <strong>Site: </strong>
                     https://www.danielsilva-dev.com.br/
                   </li>
                 </ul>
+                <p>
+                  <strong>
+                    “Developing has become a favorite hobby and eventually like
+                    to philosophize about life with a beer (🍺).”
+                  </strong>
+                </p>
               </div>
             </div>
           </div>
-          <div className="bio box">
-            <header>
-              <h4>Biography</h4>
-            </header>
-            <strong>
-              “Developing has become a favorite hobby and eventually like to
-              philosophize about life with a beer (🍺).”
-            </strong>
+          <div className="other-info">
+            <div className="followers box">
+              <header>
+                <h4>Followers</h4>
+              </header>
+              <strong>17</strong>
+            </div>
+            <div className="following box">
+              <header>
+                <h4>Following</h4>
+              </header>
+              <strong>31</strong>
+            </div>
+            <div className="starreds box">
+              <header>
+                <h4>Starreds</h4>
+              </header>
+              <strong>20</strong>
+            </div>
+            <div className="repos box">
+              <header>
+                <h4>Repositories</h4>
+              </header>
+              <strong>20</strong>
+            </div>
           </div>
         </div>
       </div>
