a51323a5d3abd27f034f77f43001834bad4d95d7
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-02 12:26:54 -0300

    feat: implementando a tela login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,18 @@
+import { LoginForm } from "../../components/LoginForm";
+import background from "../../assets/background-flowers.png";
+
 import "./styles.scss";
 
 export function LoginPage() {
   return (
-    <section>
-      <h1>Login Page</h1>
+    <section
+      className="login-page-content"
+      style={{
+        background: `url(${background}) no-repeat left top`,
+      }}
+    >
+      <LoginForm />
+      <footer className="copyright">© Health Track • 2021</footer>
     </section>
   );
 }
