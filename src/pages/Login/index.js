110cfb0ce9bab37fcbc7154ccf851fa3dbcb50f8
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-03 13:09:12 -0300

    feat: implementando a função que redireciona para o form de login ou singup

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,8 +3,15 @@ import { SingupForm } from "../../components/SingupForm";
 import background from "../../assets/background-flowers.png";
 
 import "./styles.scss";
+import { useState } from "react";
 
 export function LoginPage() {
+  const [isShow, setIsShow] = useState(true);
+
+  function onShow() {
+    setIsShow(!isShow);
+  }
+
   return (
     <section
       className="login-page-content"
@@ -12,14 +19,14 @@ export function LoginPage() {
         background: `url(${background}) no-repeat left top`,
       }}
     >
-      {false ? <LoginForm /> : <SingupForm />}
-      {false ? (
+      {isShow ? <LoginForm /> : <SingupForm />}
+      {isShow ? (
         <p>
-          Do you have an account? <span>Log in</span>
+          Do you have an account? <span onClick={onShow}>Log in</span>
         </p>
       ) : (
         <p>
-          Don't have an account? <span>Sing out</span>
+          Don't have an account? <span onClick={onShow}>Sing out</span>
         </p>
       )}
       <footer className="copyright">© Health Track • 2021</footer>
