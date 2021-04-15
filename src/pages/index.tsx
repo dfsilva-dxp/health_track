f74b74c122003d73739792f8a55f878ce5238422
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-15 19:13:26 -0300

    feat: implementando a tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +9,20 @@ export default function Home() {
       <Head>
         <title>Health Track | Welcome</title>
       </Head>
-      <h1>APP</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            Let us take care of your <span>health!</span>
+          </h1>
+          <p>
+            Get full access <br />
+            <span>for R9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl codding" />
+      </main>
     </>
   );
 }
